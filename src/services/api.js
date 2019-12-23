83405a7fb4a65d5fb024b8fa8e1fcc33f2daa9ed
rafael05 <rafaeloliveirasilva05@gmail.com>
2019-12-23 17:31:44 -0300

    modify http request for movie api https

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`
+  baseURL: `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`
 })
